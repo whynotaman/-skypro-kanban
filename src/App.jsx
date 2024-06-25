39d1075a3b2c601022432f399b2bfde1c0ae5968
toxicgipsy <amanarazmedov@gmail.com>
2024-06-26 00:39:02 +0300

    hooks first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,14 @@ import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Main from "./components/Main/Main.jsx";
 import PopBrowse from "./components/PopBrowse/PopBrowse.jsx";
+import PopExit from "./components/PopExit/PopExit.jsx";
 import PopNewCard from "./components/PopNewCard/PopNewCard.jsx";
 
 function App() {
   return (
     <>
       <div className="wrapper">
-        {/* pop-up start*/}
-        <div className="pop-exit" id="popExit">
-          <div className="pop-exit__container">
-            <div className="pop-exit__block">
-              <div className="pop-exit__ttl">
-                <h2>Выйти из аккаунта?</h2>
-              </div>
-              <form className="pop-exit__form" id="formExit" action="#">
-                <div className="pop-exit__form-group">
-                  <button className="pop-exit__exit-yes _hover01" id="exitYes">
-                    <a href="modal/signin.html">Да, выйти</a>{" "}
-                  </button>
-                  <button className="pop-exit__exit-no _hover03" id="exitNo">
-                    <a href="main.html">Нет, остаться</a>{" "}
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
+        <PopExit />
         <PopNewCard />
         <PopBrowse />
         {/* pop-up end*/}
